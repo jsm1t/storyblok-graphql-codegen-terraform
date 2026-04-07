5487cb830cbd73a5ae953100b689c1d20176e268
Joost Smit <19669805+jsm1t@users.noreply.github.com>
2026-04-07 15:17:14 +0200

    chore: upgrade outdated dependencies (#28)

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
+import path from 'path'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      graphql: path.resolve(__dirname, 'node_modules/graphql/index.js'),
+    },
+  },
   test: {
     include: ['**/*.test.ts'],
-    deps: {
-      fallbackCJS: true,
-    },
   },
 })
